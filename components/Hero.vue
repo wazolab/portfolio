ae922f7d7ea1d74593d963d12448fa6de1edd639
Noé Viricel <noe.viricel@wazo-lab.io>
2020-10-26 15:39:43 +0100

    Create Hero component

---
+++ components/Hero.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="rounded-3xl bg-white w-1/2 mx-auto p-6 shadow-xl text-center">
+    <Logo />
+    <h1 class="font-mono text-2xl">
+      wazo-lab.io
+    </h1>
+    <div class="mt-4">
+      <a
+        href="#projects"
+        target="_self"
+        rel="noopener noreferrer"
+        class="button--green"
+      >
+        Projects
+      </a>
+      <a
+        href="#contact-me"
+        target="_self"
+        rel="noopener noreferrer"
+        class="button--grey"
+      >
+        Contact Me
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
