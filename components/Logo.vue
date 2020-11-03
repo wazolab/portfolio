449f7778c054c6fb4359613a4ab9745f2a6090b7
Noé Viricel <noe.viricel@wazo-lab.io>
2020-11-03 19:26:55 +0100

    Increases Logo responsivness

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
   <svg
-    class="brand-logo w-24 md:w-40"
+    class="brand-logo w-24 md:w-32"
     xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink"
     viewBox="0 0 230 230"
@@ -197,6 +197,10 @@
     </g>
   </svg>
 </template>
+
+<script>
+export default {};
+</script>
 
 <style>
 .brand-logo {
